c33e7cc6eaf4dba38e571c40b152a5e862ae5468
inesglagla <100957824+inesglagla@users.noreply.github.com>
2022-06-24 13:22:54 +0200

    product.js est terminé, ajout de cart.js

--- front/js/product.js
+++ front/js/product.js
@@ -31,10 +31,79 @@ fetch('http://localhost:3000/api/products/'+id)
         for (let i = 0; i < colorsList.length; i++) {
         let productCouleur = document.createElement('option');
         productCouleur.innerHTML = products.colors[i];
+            //Sauvegarder le choix de la couleur dans une variable
+            for (let i = 0; i < `${products.colors[i]}`.length; i++) {
+                let buttonColor = productCouleurPlacement;
+                buttonColor.addEventListener('input', (ec) => {
+                    colorItem = ec.target.value;
+                    console.log(colorItem);
+                })
+            }
+        //Fermeture de la partie couleur
         productCouleurPlacement.appendChild(productCouleur);
         }
+        //Choisir la quantité du produit
+        let productQuantité = document.getElementsByName('itemQuantity');
+
+        for (let i = 0; i < productQuantité.length; i++) {
+            let buttonQuantity = productQuantité[i];
+            buttonQuantity.addEventListener('input', (eq) => {
+                productQuantityAll = eq.target.value;
+                console.log(productQuantityAll);
+            })
+        }
+    //Ajouter le produit au panier
+        //Créer les variables principales avant l'interaction
+        let productColor;
+        let productQuantity;
+        //Créer les variables qui contiendront les choix
+        let colorItem;
+        let productQuantityAll;
+        //Interagir avec le bouton pour ajouter au panier
+        const addCart = document.querySelector("#addToCart")
+        addCart.addEventListener('click', e => {
+            //Configuration du produit pour le stocker
+            let productData = {
+            productID : products._id,
+            productColor : colorItem,
+            productQuantity : productQuantityAll
+            }
+            //Ajout dans du produit dans le localStorage, avec un array servant de panier
+            let addProductCart = JSON.parse(localStorage.getItem("Panier"));
+            //Condition si une couleur et une quantité n'ont pas été choisies
+            if (productQuantityAll == undefined || productQuantityAll < 1 || productQuantityAll > 100 || colorItem == undefined || productQuantityAll && colorItem == undefined) {
+                alert('Il faut choisir une quantité entre 1 et 100! Il faut aussi choisir une couleur!');
+            //Si le panier n'existe pas, on le crée avec le premier produit
+            } else if (addProductCart == null) {
+                    addProductCart = [];
+                    addProductCart.push(productData);
+                    localStorage.setItem("Panier", JSON.stringify(addProductCart));
+                    console.log(addProductCart);
+            //Si le panier existe, on suit la condition suivante
+            } else {
+                    //Variable pour montrer que le panier existe et contient un produit au minimum
+                    productsAdded = JSON.parse(localStorage.getItem("Panier"));
+                    if (productsAdded) {
+                        for (let choice of productsAdded) {
+                            //On vérifie si l'ID du produit et sa couleur existent déjà dans le panier
+                            if (choice.productID == products._id && choice.productColor == colorItem) {
+                                //Si ils sont pareils, on ajoute uniquement la quantité au panier
+                                let addQuantity = parseInt(choice.productQuantity) + parseInt(productQuantityAll);
+                                choice.productQuantity = JSON.stringify(addQuantity);
+                                console.log("On a ajouté la nouvelle quantité");
+                                return (localStorage.Panier = JSON.stringify(productsAdded));
+                        } //Si la couleur et l'ID sont différents, on ajoute le produit au panier
+                        else (addProductCart == true); {
+                        addProductCart.push(productData);
+                        localStorage.setItem("Panier", JSON.stringify(addProductCart));
+                        console.log("On a ajouté le nouveau produit")
+                        }
+                    }
+                }
+            }
+        })
     })
     //Message d'erreur
     .catch((error) => {
-        console.log("Il y a un soucis concernant le chargement de l'article." + error);
+        console.log("Il y a un soucis concernant le chargement de l'article :" + error);
     });
